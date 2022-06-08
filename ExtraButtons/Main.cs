7d9ca08014f330aeaf7ebaf2c740d03933079c2c
Om3ga <carriermatthew25@outlook.com>
2022-06-07 22:14:08 -0400

    Ready buttons seem to be working now sheesh

--- ExtraButtons/Main.cs
+++ ExtraButtons/Main.cs
@@ -3,6 +3,7 @@ using BepInEx.IL2CPP;
 using BepInEx.Logging;
 using HarmonyLib;
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace ExtraButtons
@@ -11,7 +12,7 @@ namespace ExtraButtons
     [BepInProcess("Among Us.exe")]
     public class Main : BasePlugin
     {
-
+        
         public const string Version = "1.0.0";
         public const string Id = "ExtraButtons.pack";
         public Harmony Harmony { get; } = new Harmony(Id);
@@ -24,46 +25,94 @@ namespace ExtraButtons
         [HarmonyPatch(typeof(LobbyBehaviour), nameof(LobbyBehaviour.Start))]
         public class LobbyPatch
         {
-            public static void Prefix(HudManager __instance)
+            public static void Prefix()
+            {
+
+                CreateReadyButtons();
+            }
+
+
+            public static void CreateReadyButtons()
             {
                 var CHLog = new ManualLogSource("ExtraButtons");
                 BepInEx.Logging.Logger.Sources.Add(CHLog);
                 try
                 {
+
                     CHLog.Log(LogLevel.Info, "Starting creation of buttons");
+
                     var ReadyButton = UnityEngine.Object.Instantiate(HudManager.Instance.UseButton, HudManager.Instance.UseButton.transform.parent);
+                    UnityEngine.Object.Destroy(ReadyButton.GetComponentInChildren<TextTranslatorTMP>());
+                    CHLog.Log(LogLevel.Info, "Button Grabbed");
+                    ReadyButton.name = "ReadyButton";
+                    CHLog.Log(LogLevel.Info, "button name set");
                     ReadyButton.OverrideText("Ready");
+                    CHLog.Log(LogLevel.Info, "button text set");
                     ReadyButton.OverrideColor(color: Color.red);
+                    CHLog.Log(LogLevel.Info, "button color set");
                     ReadyButton.transform.localPosition = new Vector3((float)ReadyButton.transform.localPosition.x - 2f, (float)ReadyButton.gameObject.transform.localPosition.y, (float)ReadyButton.gameObject.transform.position.z);
+                    CHLog.Log(LogLevel.Info, "button position set");
                     ReadyButton.graphic.SetCooldownNormalizedUvs();
-                    ReadyButton.currentTarget = PlayerControl.LocalPlayer.closest;
-                    var currentName = PlayerControl.LocalPlayer.name;
+                    CHLog.Log(LogLevel.Info, " button cooldown set");
                     var passiveButton = ReadyButton.GetComponent<PassiveButton>();
+                    CHLog.Log(LogLevel.Info, "passive button component Grabbed");
                     passiveButton.OnClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+                    CHLog.Log(LogLevel.Info, "onclick event created");
                     passiveButton.OnClick.AddListener((Action)(() =>
                     {
-                        PlayerControl.LocalPlayer.RpcSetName($"<color=green>{currentName}");
-                        if (ReadyButton.buttonLabelText.text == "Ready")
-                        {
 
-                            ReadyButton.OverrideText("UnReady");
-                            ReadyButton.OverrideColor(Color.green);
+                        var currentName = PlayerControl.LocalPlayer.name;
+                        if (currentName.Contains(">"))
+                        {
+                            var modifiedName = currentName.Split(">", StringSplitOptions.RemoveEmptyEntries);
+                            if (ReadyButton.buttonLabelText.text == "Ready")
+                            {
+                                CHLog.Log(LogLevel.Info, $"Current name : {modifiedName[1]}");
+                                PlayerControl.LocalPlayer.CheckName($"<color=green>{modifiedName[1]}");
+                                CHLog.Log(LogLevel.Info, "Button Clicked player set to green");
+                                ReadyButton.OverrideText("UnReady");
+                                ReadyButton.OverrideColor(Color.green);
+                            }
+                            else
+                            {
+                                CHLog.Log(LogLevel.Info, $"Current name : {modifiedName[1]}");
+                                CHLog.Log(LogLevel.Info, "Button Clicked player set to red");
+                                CHLog.Log(LogLevel.Info, $"Button text {ReadyButton.buttonLabelText.text}");
+                                PlayerControl.LocalPlayer.CheckName($"<color=red>{modifiedName[1]}");
+                                ReadyButton.OverrideText("Ready");
+                                ReadyButton.OverrideColor(Color.red);
+                            }
                         }
                         else
                         {
-                            PlayerControl.LocalPlayer.RpcSetName($"<color=red>{currentName}");
-                            ReadyButton.OverrideText("Ready");
-                            ReadyButton.OverrideColor(Color.red);
+                            if (ReadyButton.buttonLabelText.text == "Ready")
+                            {
+                                PlayerControl.LocalPlayer.CheckName($"<color=green>{currentName}");
+                                CHLog.Log(LogLevel.Info, "Button Clicked player set to green");
+                                ReadyButton.OverrideText("UnReady");
+                                ReadyButton.OverrideColor(Color.green);
+                            }
+                            else
+                            {
+                                CHLog.Log(LogLevel.Info, "Button Clicked player set to red");
+                                CHLog.Log(LogLevel.Info, $"Button text {ReadyButton.buttonLabelText.text}");
+                                PlayerControl.LocalPlayer.CheckName($"<color=red>{currentName}");
+                                ReadyButton.OverrideText("Ready");
+                                ReadyButton.OverrideColor(Color.red);
+                            }
                         }
+                        
+                        
+                        
                     }));
+
+
                 }
                 catch (Exception e)
                 {
                     CHLog.Log(LogLevel.Error, $"{e.InnerException.StackTrace}");
                     throw;
                 }
-               
-
             }
         }
 
@@ -83,66 +132,65 @@ namespace ExtraButtons
                 UnityEngine.Object.Destroy(ReadyButton);
                 CHLog.Log(LogLevel.Info, "ReadyButton Destroyed.");
 
-                PlayerControl.LocalPlayer.RpcSetName($"<color=white>{currentName}");
+                PlayerControl.LocalPlayer.CheckName($"<color=white>{currentName}");
             }
         }
 
-        //[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Start))]
-        //public class OnMeetingStart
-        //{
-        //    public static void Prefix(MeetingHud __instance)
-        //    {
-        //        var RaiseLowerHandButton = UnityEngine.Object.Instantiate(HudManager.Instance.UseButton, HudManager.Instance.UseButton.transform.parent);
-        //        UnityEngine.Object.Destroy(RaiseLowerHandButton.GetComponentInChildren<TextTranslatorTMP>());
-        //        RaiseLowerHandButton.transform.localPosition = new Vector3((float)RaiseLowerHandButton.transform.localPosition.x - 2f, (float)RaiseLowerHandButton.gameObject.transform.localPosition.y, (float)RaiseLowerHandButton.gameObject.transform.position.z);
-        //        RaiseLowerHandButton.name = "RaiseHandButton";
-        //        RaiseLowerHandButton.OverrideText("Raise Hand");
-        //        RaiseLowerHandButton.Show();
-        //        RaiseLowerHandButton.enabled = true;
-        //        RaiseLowerHandButton.Awake();
-        //        RaiseLowerHandButton.graphic.SetCooldownNormalizedUvs();
-        //        var currentName = PlayerControl.LocalPlayer.name;
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Start))]
+        public class OnMeetingStart
+        {
+            public static void Prefix(MeetingHud __instance)
+            {
+                var RaiseLowerHandButton = UnityEngine.Object.Instantiate(HudManager.Instance.UseButton, HudManager.Instance.UseButton.transform.parent);
+                UnityEngine.Object.Destroy(RaiseLowerHandButton.GetComponentInChildren<TextTranslatorTMP>());
+                RaiseLowerHandButton.transform.localPosition = new Vector3((float)RaiseLowerHandButton.transform.localPosition.x - 5f, (float)RaiseLowerHandButton.gameObject.transform.localPosition.y, (float)RaiseLowerHandButton.gameObject.transform.position.z);
+                RaiseLowerHandButton.name = "RaiseHandButton";
+                RaiseLowerHandButton.OverrideText("Raise Hand");
+                RaiseLowerHandButton.Show();
+                RaiseLowerHandButton.enabled = true;
+                RaiseLowerHandButton.Awake();
+                RaiseLowerHandButton.graphic.SetCooldownNormalizedUvs();
+                var currentName = PlayerControl.LocalPlayer.name;
 
-        //        var passiveButton = RaiseLowerHandButton.GetComponent<PassiveButton>();
-        //        passiveButton.OnClick = new UnityEngine.UI.Button.ButtonClickedEvent();
-        //        passiveButton.OnClick.AddListener((Action)(() =>
-        //        {
+                var passiveButton = RaiseLowerHandButton.GetComponent<PassiveButton>();
+                passiveButton.OnClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+                passiveButton.OnClick.AddListener((Action)(() =>
+                {
 
-        //            PlayerControl.LocalPlayer.SetName($"<color=blue>{currentName} Hand Raised!");
-        //            PlayerControl.LocalPlayer.nameText.name = $"<color=blue>{currentName} Hand Raised!";
-        //            PlayerControl.LocalPlayer.RpcSendChat($"{currentName} raised hand");
-        //            if (RaiseLowerHandButton.buttonLabelText.text == "Raise Hand")
-        //            {
-        //                RaiseLowerHandButton.OverrideText("Lower Hand");
-        //                PlayerControl.LocalPlayer.RpcSendChat($"{currentName} lowered hand");
-        //            }
-        //            else
-        //            {
-        //                RaiseLowerHandButton.OverrideText("Raise Hand");
-        //                PlayerControl.LocalPlayer.SetName(currentName);
+                    PlayerControl.LocalPlayer.CheckName($"<color=blue>{currentName} Hand Raised!");
+                    PlayerControl.LocalPlayer.nameText.name = $"<color=blue>{currentName} Hand Raised!";
+                    PlayerControl.LocalPlayer.RpcSendChat($"{currentName} raised hand");
+                    if (RaiseLowerHandButton.buttonLabelText.text == "Raise Hand")
+                    {
+                        RaiseLowerHandButton.OverrideText("Lower Hand");
+                    }
+                    else
+                    {
+                        RaiseLowerHandButton.OverrideText("Raise Hand");
+                        PlayerControl.LocalPlayer.RpcSendChat($"{currentName} lowered hand");
 
-        //            }
-        //        }
-        //        ));
+                    }
+                }
+                ));
 
 
-        //        }
-        //}
+            }
+        }
 
-        //[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Close))]
-        //public class OnMeetingDestroy
-        //{
-        //    public static void Postfix(MeetingHud __instance)
-        //    {
-        //        GameObject LowerHandButton;
-        //        GameObject RaiseHandButton;
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Close))]
+        public class OnMeetingDestroy
+        {
+            public static void Postfix(MeetingHud __instance)
+            {
+                GameObject LowerHandButton;
+                GameObject RaiseHandButton;
 
-        //        LowerHandButton = GameObject.Find("LowerHandButton");
-        //        RaiseHandButton = GameObject.Find("RaiseHandButton");
-        //        UnityEngine.Object.Destroy(LowerHandButton);
-        //        UnityEngine.Object.Destroy(RaiseHandButton);
-        //    }
-        //}
+                LowerHandButton = GameObject.Find("LowerHandButton");
+                RaiseHandButton = GameObject.Find("RaiseHandButton");
+                UnityEngine.Object.Destroy(LowerHandButton);
+                UnityEngine.Object.Destroy(RaiseHandButton);
+            }
+        }
 
     }
 }
